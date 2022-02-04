3c90dbeba1566570a528d80a3c7ee49bd09d5945
Rahul <brahul2401@gmail.com>
2022-02-04 12:06:21 +0530

    updated AppointmentController

--- src/main/java/com/upgrad/bookmyconsultation/controller/AppointmentController.java
+++ src/main/java/com/upgrad/bookmyconsultation/controller/AppointmentController.java
@@ -2,16 +2,13 @@ package com.upgrad.bookmyconsultation.controller;
 
 import com.upgrad.bookmyconsultation.entity.Appointment;
 import com.upgrad.bookmyconsultation.exception.InvalidInputException;
-import com.upgrad.bookmyconsultation.exception.SlotUnavailableException;
+import com.upgrad.bookmyconsultation.repository.AppointmentRepository;
 import com.upgrad.bookmyconsultation.service.AppointmentService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/appointments")
@@ -20,12 +17,22 @@ public class AppointmentController {
 	@Autowired
 	private AppointmentService appointmentService;
 
+	AppointmentRepository appointmentRepository;
 
 	//create a method post method named bookAppointment with return type ReponseEntity
 		//method has paramter of type Appointment, use RequestBody Annotation for mapping
 	
 		//save the appointment details to the database and save the response from the method used
 		//return http response using ResponseEntity
+
+	@PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+	public ResponseEntity bookAppointment(@RequestBody Appointment reqAppointment) throws InvalidInputException {
+
+String savedAppointment = appointmentService.appointment(reqAppointment);
+
+
+		return new ResponseEntity(savedAppointment, HttpStatus.CREATED);
+	}
 	
 	
 	
@@ -36,7 +43,16 @@ public class AppointmentController {
 		//get the appointment details using the appointmentId
 		//save the response
 		//return the response as an http response
-	
-	
+
+
+	//My code compeleted
+	@GetMapping("/appointmentsId/{appointmentId}")
+	public ResponseEntity getAppointment(@PathVariable String appointmentId){
+
+		Appointment appointmentDetails = appointmentService.getAppointment(appointmentId);
+
+		appointmentRepository.save(appointmentDetails);
+		return new ResponseEntity(appointmentDetails, HttpStatus.CREATED);
+	}
 
 }
