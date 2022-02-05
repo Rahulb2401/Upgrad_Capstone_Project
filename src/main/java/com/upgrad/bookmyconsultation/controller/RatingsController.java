6311dd8a7bac265158b7bd768d60637a51a92695
Rahul <brahul2401@gmail.com>
2022-02-05 22:14:05 +0530

    updated RatingsController class

--- src/main/java/com/upgrad/bookmyconsultation/controller/RatingsController.java
+++ src/main/java/com/upgrad/bookmyconsultation/controller/RatingsController.java
@@ -3,6 +3,7 @@ package com.upgrad.bookmyconsultation.controller;
 import com.upgrad.bookmyconsultation.entity.Rating;
 import com.upgrad.bookmyconsultation.service.RatingsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -12,15 +13,23 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class RatingsController {
 
-	@Autowired
-	private RatingsService ratingsService;
+    @Autowired
+    private RatingsService ratingsService;
 
 
-	//create a post method named submitRatings with return type as ResponseEntity
-		//define the method parameter rating of type Rating, use @RequestBody for mapping
-		
-		//submit the ratings
-	
-		//return http response with status set to OK
-	
+    //create a post method named submitRatings with return type as ResponseEntity
+    //define the method parameter rating of type Rating, use @RequestBody for mapping
+
+    //submit the ratings
+
+    //return http response with status set to OK
+
+    @PostMapping(value = "submitrating")
+    public ResponseEntity submitRatings(@RequestBody Rating rating) {
+
+        ratingsService.submitRatings(rating);
+
+        return new ResponseEntity(HttpStatus.OK);
+    }
+
 }
