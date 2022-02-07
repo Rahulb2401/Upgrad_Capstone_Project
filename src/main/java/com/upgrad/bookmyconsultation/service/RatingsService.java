14c02c991ca876d157ed35f8ab81582cd5a93e76
Rahul <brahul2401@gmail.com>
2022-02-07 13:14:54 +0530

    updated RatingsService class by saving the avg rating of doctor

--- src/main/java/com/upgrad/bookmyconsultation/service/RatingsService.java
+++ src/main/java/com/upgrad/bookmyconsultation/service/RatingsService.java
@@ -8,32 +8,71 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Service;
 
-import java.util.UUID;
-import java.util.stream.Collectors;
+import java.util.HashMap;
+import java.util.List;
 
 
 @Service
 public class RatingsService {
 
-	@Autowired
-	private ApplicationEventPublisher publisher;
+    @Autowired
+    private ApplicationEventPublisher publisher;
 
-	@Autowired
-	private RatingsRepository ratingsRepository;
+    @Autowired
+    private RatingsRepository ratingsRepository;
 
-	@Autowired
-	private DoctorRepository doctorRepository;
+    @Autowired
+    private DoctorRepository doctorRepository;
 
-	
-	//create a method name submitRatings with void return type and parameter of type Rating
-		//set a UUID for the rating
-		//save the rating to the database
-		//get the doctor id from the rating object
-		//find that specific doctor with the using doctor id
-		//modify the average rating for that specific doctor by including the new rating
-		//save the doctor object to the database
-	
-	
 
-	}
+    //create a method name submitRatings with void return type and parameter of type Rating
+    //set a UUID for the rating
+    //save the rating to the database
+    //get the doctor id from the rating object
+    //find that specific doctor with the using doctor id
+    //modify the average rating for that specific doctor by including the new rating
+    //save the doctor object to the database
+
+    public void submitRatings(Rating rating) {
+
+        Rating updateRating = new Rating();
+
+        updateRating.setId(rating.getId());
+        updateRating.setRating(rating.getRating());
+
+        ratingsRepository.save(updateRating);
+
+
+        List findRating = ratingsRepository.findByDoctorId(rating.getDoctorId());
+
+        Integer sum = 0;
+        Integer avg = 0;
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < findRating.size(); i++) {
+            map.put(i, (Integer) findRating.get(i));
+        }
+
+
+        for (int j = 0; j < map.size(); j++) {
+            if (map.containsKey(j)) {
+                sum += map.get(j);
+            }
+        }
+
+
+        avg = Integer.parseInt(String.valueOf(sum / map.size()));
+
+        updateRating.setRating(avg);
+
+
+        Doctor doctorInfo = new Doctor();
+
+        doctorInfo.setId(rating.getDoctorId());
+        doctorInfo.setRating(Double.parseDouble(String.valueOf(avg)));
+        doctorRepository.save(doctorInfo);
+
+
+    }
 }
